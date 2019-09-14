3637131b1dcd248afd971f3f44237acd0164028b
PfarsMavhs <siboiboip@gmail.com>
2019-09-14 14:17:07 +0200

    Add files via upload

---
+++ Learning-GateAway/src/main/java/com/gateaway/LearningGateAwayApplication.java
@@ -0,0 +1,17 @@
+package com.gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class LearningGateAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGateAwayApplication.class, args);
+	}
+
+}
---
+++ Learning-GateAway/src/test/java/com/gateaway/LearningGateAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGateAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
